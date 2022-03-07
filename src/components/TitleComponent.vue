c412c5810c0b667dac8d6fcf272f17cc67273e02
Pietrodon <donvitopietro00@gmail.com>
2022-03-07 17:15:41 +0100

    rinomina componente

--- src/components/TitleComponent.vue
+++ src/components/TitleComponent.vue
@@ -1,12 +1,17 @@
 <template>
- 
+    <div class="titolo">
+        <h1>La mia prima app con Vue CLI</h1>
+    </div>
 </template>
 
 <script>
-
+export default {
+  name: 'TitleComponent',
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
 
 </style>
