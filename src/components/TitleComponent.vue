c6824362560dcdbbc53e5dcd303f31f9b9662e3c
Pietrodon <donvitopietro00@gmail.com>
2022-03-07 17:24:13 +0100

    modifiche style component

--- src/components/TitleComponent.vue
+++ src/components/TitleComponent.vue
@@ -12,6 +12,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  h1{
+    background-color: black;
+    color: red;
+    padding: 20px;
+  }
+
 
 
 </style>
